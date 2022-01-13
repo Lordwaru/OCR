6c429ea7d799b024b57a8babea83f62f58872157
Warusaurus <warusaurus@waru.mx>
2022-01-12 17:19:11 -0700

    parse prototype and comparison successful

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Lordwaru/OCR/ocr"
 )
@@ -10,12 +11,47 @@ func main() {
 	var number_a ocr.OCR_number
 	var number_b ocr.OCR_number
 
-	number_a = ocr.One()
-	number_b = ocr.Two()
+	number_a.Number = make([]ocr.OCR_pattern, 3)
+
+	number_b = ocr.Zero()
+
+	data, err := os.ReadFile("input.txt")
+	check(err)
+
+	fmt.Println((len(data)))
+	str := string(data)
+
+	i := 0
+	for _, d := range str[0:3] {
+		number_a.Number[0].Cells.Characters[i] = d
+		i++
+	}
+
+	i = 0
+
+	for _, d := range str[27:30] {
+		number_a.Number[1].Cells.Characters[i] = d
+		i++
+	}
+
+	i = 0
+	for _, d := range str[54:57] {
+		number_a.Number[2].Cells.Characters[i] = d
+		i++
+	}
+
+	fmt.Println(number_a.Number)
+	fmt.Println(number_b.Number)
 
 	if ocr.Compare(number_a, number_b) {
 		fmt.Println("Yes")
 	} else {
 		fmt.Println("No")
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
--- ocr/ocr.go
+++ ocr/ocr.go
@@ -11,17 +11,21 @@ type OCR_pattern struct {
 }
 
 func Compare(A OCR_number, B OCR_number) bool {
-	for i, row := range A.Number {
-		if row.Cells.Characters[0] != B.Number[i].Cells.Characters[0] {
-			return false
+	if len(A.Number) > 0 && len(B.Number) > 0 {
+		for i, row := range A.Number {
+			if row.Cells.Characters[0] != B.Number[i].Cells.Characters[0] {
+				return false
+			}
+			if row.Cells.Characters[1] != B.Number[i].Cells.Characters[1] {
+				return false
+			}
+			if row.Cells.Characters[2] != B.Number[i].Cells.Characters[2] {
+				return false
+			}
 		}
-		if row.Cells.Characters[1] != B.Number[i].Cells.Characters[1] {
-			return false
-		}
-		if row.Cells.Characters[2] != B.Number[i].Cells.Characters[2] {
-			return false
-		}
+		return true
+	} else {
+		return false
 	}
 
-	return true
 }
